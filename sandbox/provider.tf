98b4d180b040703e4da5cd71b3f80f310b5fffaa
Islam Kambarov <ikambarov@yahoo.com>
2022-06-30 19:17:51 -0500

    Remove Provider

--- sandbox/provider.tf
+++ sandbox/provider.tf
@@ -12,9 +12,3 @@ terraform {
   required_version = ">= 0.12.20"
 }
 
-provider "aws" {
-  region              = var.aws_region
-  allowed_account_ids = var.aws_account_ids
-  profile             = "default"
-  version             = ">= 2.46.0"
-}
